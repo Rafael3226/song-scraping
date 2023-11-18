af3cc141ef0158b389c6ee056a8c87c141bf677e
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-11-17 19:57:50 -0500

    remove unused file

--- src/scrap/index.ts
+++
@@ -1,8 +0,0 @@
-import { ScrapGenre } from "./scrap-genre";
-// import { ScrapTracks } from './track'
-
-export function Scrap(body: HTMLElement) {
-  const genres = ScrapGenre(body);
-  console.log(JSON.stringify(genres));
-  // await ScrapTracks(webLoader)
-}
